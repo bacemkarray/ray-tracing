ddb0fd28e27f844a938b067bc1ca1b75c3da79ba
bacemkarray <bkarray1@gmail.com>
2026-03-26 12:53:01 -0400

    replaced random_utils with relevant functions

---
+++ cuheaders/random_utils.cuh
@@ -0,0 +1,25 @@
+#ifndef RANDOMUTILS_H
+#define RANDOMUTILS_H
+
+#include <stdlib.h>
+#include "vec3.cuh"
+
+#define RANDVEC3 vec3(curand_uniform(local_rand_state),curand_uniform(local_rand_state),curand_uniform(local_rand_state))
+
+__device__ inline vec3 random_in_unit_sphere(curandState *local_rand_state) {
+    vec3 p;
+    do {
+        p = 2.0*RANDVEC3;
+    } while (p.squared_length() >= 1.0);
+    return p;
+}
+
+__device__ inline vec3 random_in_unit_disk(curandState *local_rand_state) {
+    vec3 p;
+    do {
+        p = 2.0*vec3(curand_uniform(local_rand_state), curand_uniform(local_rand_state), 0) - vec3(1,1,0);
+    } while (dot(p,p) >= 1.0);
+    return p;
+}
+
+#endif
